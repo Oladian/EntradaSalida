0c4079fb05ab072e4a4719a76de23896381ac114
Izan <ios2asir@gmail.com>
2018-02-22 14:22:10 +0100

    Correcciones de ejercicios

--- Entrada_Salida/src/com/iesvirgendelcarmen/com/ejercicios/IOArgs.java
+++ Entrada_Salida/src/com/iesvirgendelcarmen/com/ejercicios/IOArgs.java
@@ -5,13 +5,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class IOArgs {
 	public static void main(String[] args) {
 		if (args.length!=2) {
 			System.out.println("Argumentos insuficientes");
 			return;
-		} else {
+		} 
+		if(args[0].equals(args[1])) {
+			System.out.println("Fichero origen igual que el fichero de destino.");
+			return;
+		}
+		else {
 			File inputFile = new File(args[0]);
 			File outputFile = new File(args[1]);
 			
@@ -21,13 +27,14 @@ public class IOArgs {
 			} else {
 				try (FileInputStream fileInputStream = new FileInputStream(inputFile); 
 					FileOutputStream fileOutputStream = new FileOutputStream(outputFile);) {
-					
-					int readByte;
-					while((readByte=fileInputStream.read())!=-1) {
-						fileOutputStream.write((char)readByte);
-					}
-					System.out.println("Fichero "+args[1]+" creado correctamente.");
 
+						int readByte;
+						while((readByte=fileInputStream.read())!=-1) {
+							fileOutputStream.write((char)readByte);
+						} fileOutputStream.flush();
+						System.out.println("Fichero "+args[0]+" copiado correctamente."
+								+ "en "+args[1]);
+						
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
 					System.out.println("FileNotFoundException ERROR");
